82a103d78bf10d51552d6a3456802cea192cfab6
unknown <wydewy@126.com>
2017-10-29 14:29:00 +0800

    wydewy

--- src/main/java/cn/edu/bupt/yaoxintong/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/cn/edu/bupt/yaoxintong/service/impl/AuthenticationServiceImpl.java
@@ -1,7 +1,10 @@
 package cn.edu.bupt.yaoxintong.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cn.edu.bupt.yaoxintong.dao.AuthenticationDianshangMapper;
+import cn.edu.bupt.yaoxintong.dao.AuthenticationYaoqiMapper;
 import cn.edu.bupt.yaoxintong.pojo.AuthenticationDianshang;
 import cn.edu.bupt.yaoxintong.pojo.AuthenticationYaoqi;
 import cn.edu.bupt.yaoxintong.service.AuthenticationService;
@@ -10,16 +13,21 @@ import cn.edu.bupt.yaoxintong.service.AuthenticationService;
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
+	@Autowired
+	AuthenticationYaoqiMapper authenticationYaoqiMapper;
+	@Autowired
+	AuthenticationDianshangMapper authenticationDianshangMapper;
+	
 	@Override
-	public boolean addAuthenticationYaoqi(AuthenticationYaoqi dianshang) {
+	public boolean addAuthenticationYaoqi(AuthenticationYaoqi yaoqi) {
 		// TODO Auto-generated method stub
-		return false;
+		return authenticationYaoqiMapper.insert(yaoqi)>0;
 	}
 
 	@Override
 	public boolean addAuthenticationDianshang(AuthenticationDianshang dianshang) {
 		// TODO Auto-generated method stub
-		return false;
+		return authenticationDianshangMapper.insert(dianshang)>0;
 	}
 
 }
